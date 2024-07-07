359e41280e1277b1ebd774a95a8837bc2c94210b
Thom <thom.nmaciel.baron@gmail.com>
2024-07-07 11:11:40 -0300

    leetcode [medium]: 98.ValidateBinarySearchTree Java

--- leetcode/medium/ValidateBinarySearchTree_98.java
+++ leetcode/medium/ValidateBinarySearchTree_98.java
@@ -1,3 +1,18 @@
+import leetcode.TreeNode;
+
 public class ValidateBinarySearchTree_98 {
+
+    boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    boolean isValidBST(TreeNode node, int minVal, int maxVal) {
+        if (node == null)
+            return true;
+        
+        if(node.val <= minVal || node.val >= maxVal)
+            return false;
     
+        return isValidBST(node.left, minVal, node.val) && isValidBST(node.right, node.val, maxVal);
+    }
 }
